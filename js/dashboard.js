1196d515ffa791cb56f1228c266ac0ed7dd2d44b
akanniPM <adejumotaiwo33@gmail.com>
2025-09-09 20:23:16 -0400

    new

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,5 @@
 let userDatabase = JSON.parse(localStorage.getItem('myDatabase')) || []
+let blogDatabaseMemory = JSON.parse(localStorage.getItem('blogDatabaseMemory')) || []
 
 let currentUserIndex = Number(localStorage.getItem('myUSerIndex'))
 
@@ -13,52 +14,94 @@ let currentUser = document.getElementById('currentUser')
 currentUser.innerHTML = `<h2>Welcome ${currentUserObj.username}</h2>`
 let post =document.getElementById('timeLine')
 
+let blogDatabase = JSON.parse(localStorage.getItem('blogDatabaseMemory')) || []
+
 
 function submitPost() {
+    let blogInner = document.getElementById('postContent')
     let blogInput = document.getElementById('postContent').value.trim()
     if (!blogInput) {
         // submitPost.onclick = null;
         alert('input a tweet')
         return
     }
-    if (blogInput) {
-        post.innerHTML 
+    else {
+        blogDatabase.push(blogInput)
+        localStorage.setItem('blogDatabaseMemory', JSON.stringify(blogDatabase))
+       displayBlog()
+       blogInner.value = ''
+    }
+}
+
+function displayBlog() {
+    post.innerHTML = ''
+    for (let index = 0; index < blogDatabase.length; index++) {
+        const element = blogDatabase[index];
+    
+        
+    
+     post.innerHTML 
         +=  `
-       <div class="blog">
-           <p>${blogInput}</p>
+       <div>
+           <p>${element}</p>
             <div id="tweetArea">
-                <button onclick="edit()" id="edit">Edit</button>
-                <button onclick="deleter(this)" id="delete">Delete</button>
+                <button onclick="edit(${index})" id="edit">Edit</button>
+                <button onclick="deleter(${index})" id="delete">Delete</button>
                 <hr>
             </div>
         </div>
         `
-        
-        // getTweet()
-         localStorage.setItem('posts', JSON.stringify(post.innerHTML))
-
-         blogInput=""
     }
 }
-
-let pastPost = JSON.parse(localStorage.getItem('posts'))|| ''
-
-post.innerHTML = pastPost
+displayBlog()
 
 
 
- function deleter(button) {
-    let blogDiv = button.closest(".blog");
-    let content = blogDiv.querySelector("p").innerText;
 
-    // Remove from localStorage
-    let pastPost = JSON.parse(localStorage.getItem("posts")) || [];
-    savedPosts = pastPost.filter(p => p !== content);
-    localStorage.setItem("posts", JSON.stringify(pastPost));
 
-    // Remove from DOM
-    blogDiv.remove();
-  }
+function deleter(params) {
+   let confirmDelete = confirm('are you sure you want to delete this post')
+    if (confirmDelete) {
+        blogDatabase.splice(params, 1)
+        localStorage.setItem('blogDatabaseMemory', JSON.stringify(blogDatabase))
+        displayBlog()
+        
+    }
+    
+    
+}
+
+function logOut() {
+    let confirmLogout = window.confirm('are you sure you want to logout?')
+    if (confirmLogout) {
+        localStorage.removeItem('myUSerIndex')
+        window.location.href = '../pages/login.html'
+    }
+
+
+}
+
+function edit(params) {
+   let prompter = prompt('Edit your tweet', blogDatabase[params] )
+    console.log(prompter);
+    
+   if (prompter) {
+    let newBlog = blogDatabase.splice(params, 1, `${prompter}`)
+    displayBlog()
+    console.log(blogDatabase);
+    
+   }
+    
+}
+
+
+
+
+// let arr = ['ade', 'kenny', 'tudde','bimbo','yemi','bayo']
+
+// arr.splice(1,2,'143', 'sodiki')
+// console.log(arr);
+
 
 // function getTweet(${blogInput}) {
    
